e63bfa033eae94675cac6e09ce81b21d1c015b6f
Joelson Dias <joelsondiasti@outlook.com>
2023-11-21 09:07:42 -0300

    Responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import foto from "@/img/foto.jpg";
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className="lg:w-[700px] flex flex-col lg:grid lg:grid-cols-2 shadow bg-zinc-100 h-5/6">
+    <div className="flex flex-col shadow bg-zinc-100 h-5/6 lg:grid lg:grid-cols-2 lg:w-[700px] ">
       <Image src={foto} width={350} alt="" className="h-32 object-cover lg:h-full" />
       <div className="flex flex-1 flex-col justify-between px-4 py-8 lg:px-8 lg:py-12">
         <h1 className="text-5xl font-bold text-zinc-900">Hotspot</h1>
